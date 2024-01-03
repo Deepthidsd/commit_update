dd8b808061edc82fb8ebc6c39975ee1075b6d40c
Deepthidsd <107095180+Deepthidsd@users.noreply.github.com>
2024-01-03 09:43:43 +0530

    update_excel.py

---
+++ update_excel.py
@@ -0,0 +1,29 @@
+import openpyxl
+from github import Github
+import os
+
+github_token = os.environ['GITHUB_TOKEN']
+
+repo_owner = 'your_username'
+repo_name = 'your_repository'
+
+excel_file_path = 'path/to/your/excel/file.xlsx'
+
+g = Github(github_token)
+
+repo = g.get_repo(f'{repo_owner}/{repo_name}')
+
+latest_commit = repo.get_commits()[0]
+
+commit_sha = latest_commit.sha
+commit_message = latest_commit.commit.message
+commit_author = latest_commit.commit.author.name
+commit_date = latest_commit.commit.author.date
+
+wb = openpyxl.load_workbook(excel_file_path)
+sheet = wb.active
+
+new_row = [commit_sha, commit_message, commit_author, commit_date]
+sheet.append(new_row)
+
+wb.save(excel_file_path)
